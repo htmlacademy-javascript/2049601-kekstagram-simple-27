bff2d6fe2ace6667a2c79ef993f1a043b69941c7
Elena Stepanova <tay787@yandex.ru>
2022-10-13 21:01:39 +0300

    Добавляет функцию, создающую описание для фото

--- js/main.js
+++ js/main.js
@@ -17,3 +17,43 @@ function checkStringLength (string, maxLength) {
 }
 
 checkStringLength('50', 140);
+
+// Четвертое домашее задание
+//Создание массива изначальных данных
+const id = [];
+const url = [];
+
+for (let i = 1; i <= 25; i++) {
+  id[i] = i;
+  url[i] = `photos/${ i }.jpg`;
+}
+
+const descriptions = [
+  'Кекс в лесу',
+  'Кекс едет к ветеринару',
+  'Кекс обедает',
+];
+
+const like = getRandomIntInclusive(15, 200);
+const comment = getRandomIntInclusive(0, 200);
+
+//Функция, возвращающая индекс элемента массива данных
+function getRandomArrayElement (еlement) {
+  return еlement[getRandomIntInclusive(0, еlement.length - 1)];
+}
+
+//Функция, создающая описание фотографии (builder)
+function createPhotoDescription () {
+  return {
+    id: getRandomArrayElement(id),
+    url: getRandomArrayElement(url),
+    description: getRandomArrayElement(descriptions),
+    likes: like,
+    comments: comment,
+  };
+}
+
+//Метод генерации массива заданной длины
+const randomPhotoDescription = Array.from({length: 25}, createPhotoDescription);
+
+randomPhotoDescription();
