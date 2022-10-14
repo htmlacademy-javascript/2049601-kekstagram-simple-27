f15f5e9dcbdda3e334149c1db581cc747e2f2e37
Elena Stepanova <tay787@yandex.ru>
2022-10-14 20:55:34 +0300

    Исправляет замечания по домашнему заданию 4 модуля

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+// Четвертое домашее задание
+//Создание массива изначальных данных
+const NUMBER = 25;
+const MIN_LIKES_COUNT = 15;
+const MAX_LIKES_COUNT = 200;
+const MIN_COMMENTS_COUNT = 0;
+const MAX_COMMENTS_COUNT = 200;
+const DESCRIPTIONS = [
+  'Кекс в лесу',
+  'Кекс едет к ветеринару',
+  'Кекс обедает',
+];
+const identifiers = [];
+const urls = [];
+
+for (let i = 1; i <= NUMBER; i++) {
+  identifiers.push(i);
+  urls.push(`photos/${i}.jpg`);
+}
+
 //Функция, возвращающая случайное целое число из переданного диапазона включительно - взято на MDN - https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomIntInclusive(min, max) {
   if (min >= 0 && max >= 0 && min < max) {
@@ -9,33 +29,12 @@ function getRandomIntInclusive(min, max) {
   return NaN;
 }
 
-getRandomIntInclusive(5, 8);
-
 //Функция для проверки максимальной длины строки
 function checkStringLength (string, maxLength) {
   return string.length <= maxLength;
 }
 
 checkStringLength('50', 140);
-
-// Четвертое домашее задание
-//Создание массива изначальных данных
-const id = [];
-const url = [];
-
-for (let i = 1; i <= 25; i++) {
-  id[i] = i;
-  url[i] = `photos/${ i }.jpg`;
-}
-
-const descriptions = [
-  'Кекс в лесу',
-  'Кекс едет к ветеринару',
-  'Кекс обедает',
-];
-
-const like = getRandomIntInclusive(15, 200);
-const comment = getRandomIntInclusive(0, 200);
 
 //Функция, возвращающая индекс элемента массива данных
 function getRandomArrayElement (еlement) {
@@ -45,15 +44,16 @@ function getRandomArrayElement (еlement) {
 //Функция, создающая описание фотографии (builder)
 function createPhotoDescription () {
   return {
-    id: getRandomArrayElement(id),
-    url: getRandomArrayElement(url),
-    description: getRandomArrayElement(descriptions),
-    likes: like,
-    comments: comment,
+    id: getRandomArrayElement(identifiers),
+    url: getRandomArrayElement(urls),
+    description: getRandomArrayElement(DESCRIPTIONS),
+    likes: getRandomIntInclusive(MIN_LIKES_COUNT, MAX_LIKES_COUNT),
+    comments: getRandomIntInclusive(MIN_COMMENTS_COUNT, MAX_COMMENTS_COUNT),
   };
 }
 
 //Метод генерации массива заданной длины
 const randomPhotoDescription = Array.from({length: 25}, createPhotoDescription);
 
-randomPhotoDescription();
+// eslint-disable-next-line no-console
+console.log(randomPhotoDescription);
