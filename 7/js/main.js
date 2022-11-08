54f3971a878c0faa97a9bb587594951463983706
Keks <keks@htmlacademy.ru>
2022-11-08 22:31:02 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/main.js
+++ 7/js/main.js
@@ -2,7 +2,6 @@ import {getThumbnails} from './thumbnails-rendering.js';
 import './form.js';
 import './scale.js';
 import './effects.js';
-import {updateSlider} from './slider.js';
 
 getThumbnails();
-updateSlider();
+
