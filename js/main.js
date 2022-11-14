3d56d3bfd0b7afbace22401a337a87e9c8adc08e
Elena Stepanova <tay787@yandex.ru>
2022-11-14 21:05:01 +0300

    Удаляет модуль для генерации моков, переносит функцию получения данных от сервера в специальный модуль

--- js/main.js
+++ js/main.js
@@ -1,13 +1,4 @@
-import {getThumbnails} from './thumbnails-rendering.js';
-import {showAlert} from './util.js';
+import './thumbnails-rendering.js';
 import './form.js';
 import './scale.js';
 import './effects.js';
-
-
-fetch('https://27.javascript.pages.academy/kekstagram-simple/data')
-  .then((response) => response.json())
-  .then((data) => getThumbnails(data))
-  .catch(() => {
-    showAlert('Не удалось загрузить фотографии других пользователей');
-  });
--- js/thumbnails-rendering.js
+++ js/thumbnails-rendering.js
@@ -1,3 +1,5 @@
+import {showAlert} from './util.js';
+
 //Находим контейнер для изображений от других пользователей
 const picturesList = document.querySelector('.pictures');
 
@@ -25,4 +27,12 @@ const getThumbnails = function (data) {
   picturesList.append(picturesFragment);
 };
 
-export {getThumbnails};
+/*Получаем данные о фото других пользователей от севера и отрисовываем их,
+в случае ошибки при получении таких данных,вызываем  соответствующее сообщение*/
+fetch('https://27.javascript.pages.academy/kekstagram-simple/data')
+  .then((response) => response.json())
+  .then((data) => getThumbnails(data))
+  .catch(() => {
+    showAlert('Не удалось загрузить фотографии других пользователей');
+  });
+
