4315e3f644ccb5ea85b30b72c645b0c0617b3219
Keks <keks@htmlacademy.ru>
2022-11-19 13:39:16 +0300

    :heavy_check_mark: Сборка #8

--- 8/js/creating-messages.js
+++ 8/js/creating-messages.js
@@ -1,26 +1,50 @@
-// const successButton = document.querySelector('.success__button');
-// const successMessageElement = document.querySelector('.success');
-const successMessageTempalte = document.querySelector('#success').content.querySelector('.success');
-const errorMessageTempalte = document.querySelector('#error').content.querySelector('.error');
+import {isEscapeKey} from './util.js';
 
-const getSuccessMessage = function () {
-  const successMessage = successMessageTempalte.cloneNode(true);
-  document.body.append(successMessage);
+const successMessage = document.querySelector('#success').content.querySelector('.success').cloneNode(true);
+const successButton = successMessage.querySelector('.success__button');
+const errorMessage = document.querySelector('#error').content.querySelector('.error').cloneNode(true);
+const errorButton = errorMessage.querySelector('.error__button');
+
+//Переменная, необходимая для определния элемента - окна с нужным сообщением
+let message;
+
+//Закрытие окна с сообщением по нажатию Esc (обработчик)
+const onMessageEscKeydown = (evt) => {
+  if(isEscapeKey(evt)) {
+    closeMessage();
+  }
 };
 
+//Закрытие окна с сообщением по нажатию на произвольную область, кроме самого окна с сообщением (обработчик)
+const onWindowClick = (evt) => {
+  if (evt.target !== message) {
+    closeMessage();
+  }
+};
 
-// const closeSuccessMessage = function () {
-//   successMessageElement.remove();
-// };
+//Закрытие окна с сообщением, удаление обработчиков
+function closeMessage() {
+  message.remove();
+  document.removeEventListener('keydown', onMessageEscKeydown);
+  document.removeEventListener('click', onWindowClick);
+}
 
-// successButton.addEventListener('click', () => {
-//   closeSuccessMessage();
-// });
+//Добавление окна с сообщением при удачной загрузке изображения, добавление обработчиков
+const getSuccessMessage = function () {
+  message = successMessage;
+  document.body.append(successMessage);
+  successButton.addEventListener('click', closeMessage);
+  document.addEventListener('keydown', onMessageEscKeydown);
+  document.addEventListener('click', onWindowClick);
+};
 
-
+//Добавление окна с сообщением при ошибке, добавление обработчиков
 const getErrorMessage = function () {
-  const errorMessage = errorMessageTempalte.cloneNode(true);
+  message = errorMessage;
   document.body.append(errorMessage);
+  errorButton.addEventListener('click', closeMessage);
+  document.addEventListener('keydown', onMessageEscKeydown);
+  document.addEventListener('click', onWindowClick);
 };
 
 export {getSuccessMessage, getErrorMessage};
